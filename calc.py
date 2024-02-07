643570bbd08fcc3a5acf78badbd9d88c9b90fd1b
Jackieynami <157658018+Jackieynami@users.noreply.github.com>
2024-02-07 11:28:02 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,33 @@
+num1 = int(input("Enter your num 1: "))
+op = input("Enter your operator: ")
+num2 = int(input("Enter your num 2: "))
 
+elif op == "+":
+	total = num1 + num2
+	print(f"{num1} + {num2} = {total}")
+
+elif op == "-":
+	total = num1 - num2
+	print(f"{num1} - {num2} = {num1 - num2}")
+
+elif op == "*":
+	total = num1 * num2
+	print(f"{num1} * {num2} = {num1 * num2}")
+
+elif op == "/":
+	total = num1 / num2
+	print(f"{num1} / {num2} = {num1 / num2}")
+
+elif op == "**":
+	total = num1 ** num2
+	print(f"{num1} ** {num2} = {num1 ** num2}")
+
+elif op == "//":
+	total = num1 // num2
+	print(f"{num1} // {num2} = {num1 // num2}")
+
+elif op == "%":
+	total = num1 % num2
+	print(f"{num1} % {num2} = {num1 % num2}")
+else:
+	print("you print an invalid operator, please a correct later")
